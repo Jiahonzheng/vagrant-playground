94113a683baa6bf4d5e53079262a8a8cb5c200d3
Jiahonzheng <jiahonzheng@gmail.com>
2022-02-21 21:09:53 +0800

    feat(containerd): add buildkit installation
    Add buildkit installation scripts.

--- containerd/install.sh
+++ containerd/install.sh
@@ -17,3 +17,32 @@ sudo systemctl start containerd
 wget https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION}-linux-amd64.tar.gz
 tar -zxvf nerdctl-${NERDCTL_VERSION}-linux-amd64.tar.gz nerdctl
 sudo mv nerdctl /usr/local/bin/
+
+# Install BuildKit.
+wget https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-amd64.tar.gz
+sudo tar -zxvf buildkit-v${BUILDKIT_VERSION}.linux-amd64.tar.gz -C /usr/local
+echo "
+[Unit]
+Description=BuildKit
+Documentation=https://github.com/moby/buildkit
+
+[Socket]
+ListenStream=%t/buildkit/buildkitd.sock
+
+[Install]
+WantedBy=sockets.target
+" | sudo tee /etc/systemd/system/buildkit.socket
+echo "
+[Unit]
+Description=BuildKit
+Requires=buildkit.socket
+After=buildkit.socketDocumentation=https://github.com/moby/buildkit
+
+[Service]
+ExecStart=/usr/local/bin/buildkitd --oci-worker=false --containerd-worker=true
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/buildkit.service
+sudo systemctl daemon-reload
+sudo systemctl start buildkit
