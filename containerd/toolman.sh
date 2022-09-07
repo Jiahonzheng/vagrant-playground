2a79aadead17fe2a344ce18a4b97bd3f17fecd78
Jiahonzheng <jiahonzheng@gmail.com>
2022-09-07 18:23:10 +0800

    feat(containerd): add nerdctl-full installation
    Add nerdctl-full installation.

---
+++ containerd/toolman.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname $(realpath $0))"/..
+source util/common.sh
+
+NERDCTL_VERSION=0.22.2
+
+if [[ "${USE_MIRROR}" == "1" ]]; then
+  DOWNLOAD_DOMAIN=download.fastgit.org
+else
+  DOWNLOAD_DOMAIN=github.com
+fi
+
+function install() {
+  if [[ "${USE_LOCAL}" == "" ]]; then
+    wget https://${DOWNLOAD_DOMAIN}/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz
+  fi
+  sudo tar Cxzvvf /usr/local nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz
+  sudo systemctl enable --now containerd
+}
+
+case ${1} in
+install)
+  install
+  ;;
+*)
+  echo "Error: Cannot execute \"${1}\" command."
+  ;;
+esac
