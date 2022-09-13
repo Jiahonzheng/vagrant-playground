bf47c0a5875adafcec868e46ce20d889ab4e1d40
Jiahonzheng <jiahonzheng@gmail.com>
2022-09-13 22:33:38 +0800

    feat(nginx): update installation scripts
    Add NAMESPACE and NETWORK environment variables.

--- nginx/toolman.sh
+++ nginx/toolman.sh
@@ -6,17 +6,19 @@ cd "$(dirname $(realpath $0))"/..
 source util/common.sh
 
 NAME=nginx
-DATA_DIR=${HOME}/data/${NAME}
+NAMESPACE="${NAMESPACE:-default}"
+DATA_DIR=${HOME}/data/${NAMESPACE}/${NAME}
+NETWORK="${NETWORK:-bridge}"
 
 function install() {
-  mkdir -p ${DATA_DIR}/log
-
   cp -r nginx/data/* ${DATA_DIR}
 
   sudo nerdctl \
     run \
+    --namespace ${NAMESPACE} \
     -d \
     --name ${NAME} \
+    --network ${NETWORK} \
     -p 80:80 \
     -p 443:443 \
     -v ${DATA_DIR}/nginx.conf:/etc/nginx/nginx.conf \
