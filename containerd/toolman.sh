a238c3deb95ef902ca60ea5b89ada04d01660b09
Jiahonzheng <jiahonzheng@gmail.com>
2022-09-19 06:15:13 +0000

    feat(containerd): add upgrade scripts
    Add upgrade scripts.

--- containerd/toolman.sh
+++ containerd/toolman.sh
@@ -22,9 +22,22 @@ function install() {
   sudo systemctl enable --now containerd
 }
 
+function upgrade() {
+  if [[ "${USE_LOCAL}" == "" ]]; then
+    wget https://${DOWNLOAD_DOMAIN}/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz
+  fi
+  sudo tar Cxzvvf /usr/local nerdctl-full-${NERDCTL_VERSION}-linux-amd64.tar.gz
+  sudo cp /usr/local/lib/systemd/system/*.service /etc/systemd/system/
+  sudo systemctl restart containerd
+  sudo systemctl enable containerd
+}
+
 case ${1} in
 install)
   install
+  ;;
+upgrade)
+  upgrade
   ;;
 *)
   echo "Error: Cannot execute \"${1}\" command."
