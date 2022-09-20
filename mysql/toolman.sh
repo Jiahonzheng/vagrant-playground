2b75d85bd6c69dd6b5bac21ec08adec5e75744b8
Jiahonzheng <jiahonzheng@gmail.com>
2022-09-20 15:41:02 +0000

    feat(mysql): update installation scripts
    Use podman instead of nerdctl.

--- mysql/toolman.sh
+++ mysql/toolman.sh
@@ -6,24 +6,27 @@ cd "$(dirname $(realpath $0))"/..
 source util/common.sh
 
 NAME=mysql
-DATA_DIR=${HOME}/data/${NAME}
+NAMESPACE="${NAMESPACE:-default}"
+DATA_DIR=${HOME}/data/${NAMESPACE}/${NAME}
 MYSQL_VERSION=8.0
+NETWORK="${NETWORK:-podman}"
 
 function install() {
   check_env PASSWORD
 
   mkdir -p ${DATA_DIR}/conf ${DATA_DIR}/data ${DATA_DIR}/logs
 
-  sudo nerdctl \
+  podman \
     run \
     -d \
-    --name ${NAME} \
+    --name ${NAMESPACE}.${NAME} \
+    --network ${NETWORK} \
     -p 3306:3306 \
     -v ${DATA_DIR}/conf:/etc/mysql/conf.d \
     -v ${DATA_DIR}/data:/var/lib/mysql \
     -v ${DATA_DIR}/logs:/logs \
     -e MYSQL_ROOT_PASSWORD=${PASSWORD} \
-    mysql:${MYSQL_VERSION}
+    docker.io/mysql:${MYSQL_VERSION}
 }
 
 case ${1} in
